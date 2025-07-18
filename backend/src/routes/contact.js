0f3773d3c01c38cd3feff827d81b19c3514c2db2
annaellejoseemegane13 <annaellejoseemegane13@gmail.com>
2025-07-18 16:56:08 +0200

    modification

--- backend/src/routes/contact.js
+++
@@ -1,7 +0,0 @@
-// const express = require('express');
-// const router = express.Router();
-// const { sendMessage } = require('../controllers/contactController');
-
-// router.post('/send', sendMessage);
-
-// module.exports = router;
